e7a293cdba6f21f4fdff0eac95feba291b310a51
Dmitriy <108478565+pomidop4uk@users.noreply.github.com>
2025-04-19 18:18:48 +0400

    change price on fill

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const btnChoose1 = document.getElementById('btnChoose1')
 const btnChoose2 = document.getElementById('btnChoose2')
 const result1 = document.getElementById('result')
 const price1 = document.getElementById('price')
-const baliz92 = 54.50
-const baliz95 = 58.50
+const baliz92 = 55.00
+const baliz95 = 59.00
 let whoChooseBtn = ''
 let resultBtnStatus = false
 
